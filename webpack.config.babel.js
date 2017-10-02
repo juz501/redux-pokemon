fb4dff14e7eea3fde5a3a5428a83417d2ff26136
Julian Chan <julian@Julians-MacBook-Pro.local>
2017-10-02 23:13:07 +1100

    style: fix eslint issues

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -6,66 +6,66 @@ module.exports = [{
   entry: path.join(__dirname, 'src', 'client', 'client.js'),
   output: {
     path: path.join(__dirname, 'build'),
-    filename: 'bundle.js'
+    filename: 'bundle.js',
   },
-  target: "web",  
+  target: 'web',
   module: {
     loaders: [
-      { 
+      {
         test: /\.css$/,
         exclude: /(node_modules|bower_components)/,
-        loader: 'style!css' 
+        loader: 'style!css',
       },
-      { 
+      {
         test: /\.json$/,
         exclude: /(node_modules|bower_components)/,
-        loader: 'json-loader'
+        loader: 'json-loader',
       },
-      { 
+      {
         test: /\.jsx?$/,
         resolve: {
-          extensions: ['', '.js', '.jsx']
+          extensions: ['', '.js', '.jsx'],
         },
         exclude: /(node_modules|bower_components)/,
         loader: 'babel',
         query: {
-          presets: ['es2015', 'react']
-        }
+          presets: ['es2015', 'react'],
+        },
       },
-    ]
+    ],
   },
   plugins: [
     new webpack.DefinePlugin({
       'process.env': {
-        NODE_ENV: JSON.stringify('production')
-      }
+        NODE_ENV: JSON.stringify('production'),
+      },
     }),
-    new webpack.optimize.UglifyJsPlugin()
-  ]
+    new webpack.optimize.UglifyJsPlugin(),
+  ],
 },
 {
   entry: path.join(__dirname, 'src', 'server', 'server.js'),
   output: {
     path: __dirname,
-    filename: 'index.js'
+    filename: 'index.js',
   },
   target: 'node',
   externals: [nodeExternals()],
   module: {
     loaders: [
-      { 
+      {
         test: /\.json$/,
         exclude: /(node_modules|bower_components)/,
-        loader: 'json-loader'
-      },    
-      { 
-        test: /\.js$/,         
+        loader: 'json-loader',
+      },
+      {
+        test: /\.js$/,
         exclude: /(node_modules|bower_components)/,
         loader: 'babel-loader',
         query: {
-          presets: ['es2015', 'react']
-        }
+          presets: ['es2015', 'react'],
+        },
       },
-    ]
-  }
+    ],
+  },
 }];
